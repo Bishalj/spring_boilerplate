e21912f3f23588f28548c8053e8465839de2d2c1
Bishalj <bishal.jaiswal@paytminsurance.co.in>
2020-10-31 12:47:09 +0530

    inital commit

---
+++ src/test/java/com/bishal/spring/boilerplate/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishal.spring.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
